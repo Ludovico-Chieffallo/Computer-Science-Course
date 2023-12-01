9efc5257668f7b09ad8d4cecb1cd4150ae1cff06
Ludovico Chieffallo <79975866+Ludovico-Chieffallo@users.noreply.github.com>
2023-12-01 16:41:11 +0100

    Update 1_Magic_8_Ball.py

--- 1_Magic_8_Ball.py
+++ 1_Magic_8_Ball.py
@@ -1,3 +1,20 @@
+#The Magic 8-Ball is a popular toy developed in the 1950s for fortune-telling or advice seeking.
+
+#Write a magic8.py Python program that can answer any “Yes” or “No” question with a different fortune each time it executes.
+
+#We’ll be using the following 9 possible answers for our Magic 8-Ball:
+
+#Yes - definitely
+#It is decidedly so
+#Without a doubt
+#Reply hazy, try again
+#Ask again later
+#Better not tell you now
+#My sources say no
+#Outlook not so good
+#Very doubtful
+
+
 import random
 random_number = random.randint(1,10)
 
